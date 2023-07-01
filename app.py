133d9f88ff7d5923298e92edb7ff91395a0fa9ce
Tanveer Khan <khantanveer.1193@gmail.com>
2023-07-01 23:29:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -114,7 +114,7 @@ t.recommendations(df)
 st.subheader('About us')
 st.write('Kitaben is an online platform that allows users to share and read books by connecting with other users from the platform. \
           The first set of recommendations that you see are based on your previous books and suggest your favorite authors. If there are no previous readings of yours, random authors are chosen. \
-          The second set of recommendations are based on your Kitaben friends list. If the list is empty, it is initialized by some random users. \
+          The second set of recommendations are based on your Kitaben users list. If the list is empty, it is initialized by some random users. \
           Finally, the last set of recommendations are based on users that have rated common books with the ones that you choose.')
 
 
